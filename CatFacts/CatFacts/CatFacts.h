4e0bb1b783012bb9f8b8ad45da83d15fe4fba211
Mukul Sharma <mukulkaushish@gmail.com>
2021-06-19 00:40:06 +0530

    Setup ios-sdk

---
+++ CatFacts/CatFacts/CatFacts.h
@@ -0,0 +1,18 @@
+//
+//  CatFacts.h
+//  CatFacts
+//
+//  Created by MUKUL SHARMA on 19/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CatFacts.
+FOUNDATION_EXPORT double CatFactsVersionNumber;
+
+//! Project version string for CatFacts.
+FOUNDATION_EXPORT const unsigned char CatFactsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatFacts/PublicHeader.h>
+
+
